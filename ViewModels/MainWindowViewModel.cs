2dabb001064b49759ba643ada8181cc84a3db6bb
Rukin Danil <rukin2018d@yandex.ru>
2021-05-07 16:04:49 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KantorLr7.Infrastructure.Commands;
+using KantorLr7.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace KantorLr7.ViewModels
+{
+	[MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+	public class MainWindowViewModel : ViewModel
+	{
+		#region Properties
+		private string _title = "Title";
+		public string Title { get => _title; set => Set(ref _title, value); }
+		#endregion
+
+		#region Commands
+
+		#endregion
+	}
+}
